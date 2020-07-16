7e37b2e19ce10c8ebdfe9ae1738575cf776c6e43
Quinn Krug <quinn.krug@gmail.com>
2020-07-16 16:07:32 -0600

    set up LinkCreate component

---
+++ client/components/link_create.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class LinkCreate extends Component {
+  render() {
+    return (
+      <form>
+
+      </form>
+    );
+  }
+}
+
+export default LinkCreate;
