237c6b4d450ceca34b1c8db14c454a8bca0155eb
Quinn Krug <quinn.krug@gmail.com>
2020-07-16 16:00:28 -0600

    set up header component

--- client/main.js
+++ client/main.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
+import Header from './components/header';
 
 import './main.html';
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.container'));
+});
