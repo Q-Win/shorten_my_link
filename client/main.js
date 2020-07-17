07c36926404879ade4620d21fbeff4c3e185fb64
Quinn Krug <quinn.krug@gmail.com>
2020-07-17 15:25:17 -0600

    add logic to link list component

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import Header from './components/header';
 import LinkCreate from './components/link_create';
 import {Links} from '../imports/collections/links';
+import LinkList from './components/link_list';
 
 import './main.html';
 
@@ -12,6 +13,7 @@ const App = () => {
     <div>
       <Header/>
       <LinkCreate/>
+      <LinkList/>
     </div>
   )
 }
